5060ed52156959a981ae6d989087a20c47ac5045
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-25 10:27:46 -0500

    commit tree dab4f5267a92de339cbb85ef0efd2745cc9572f7

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+
+const superbowlWin = (records) => { 
+    const yearWin = records.find(record => record.result === "W");
+    return yearWin ? yearWin.year : undefined;
+}
